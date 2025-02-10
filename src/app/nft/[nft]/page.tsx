73069021c7194992dd4946219483c53dc2f4ff3d
Sergey Chystiakov <anyxem@gmail.com>
2025-02-10 10:28:37 +0100

    feat: nft page template update

--- src/app/nft/[nft]/page.tsx
+++ src/app/nft/[nft]/page.tsx
@@ -35,61 +35,59 @@ export default async function Token({ params }: { params: { nft: string } }) {
     return <NotFound title={"Token not found"} subtitle={"Token not found"} />;
   }
 
+  const ipfsToHttps = (url: string) => {
+    const cleanUrl = url.replace('ipfs://', '').split('/');
+    return `https://${cleanUrl[0]}.ipfs.w3s.link/${cleanUrl[1]}`;
+  }
+
+  const imageUrl = ipfsToHttps(data.media_uri.string);
+  const iconUrl = ipfsToHttps(data.icon_uri.string);
+
+  const metadataUrl = ipfsToHttps(data.additional_metadata_uri.string);
+
   return (
     <>
       <Hero>
         <div className="max-w-6xl px-5 md:mx-auto">
           <div className="md:col-span-7">
-            <div className="text-5xl font-bold mt-4 mb-8 leading-[4rem]">NFT</div>
+            <div className="text-5xl font-bold mt-4 mb-8 leading-[4rem]">{data.name.string} <span className="text-sm p-0 m-0">non-fungible token</span></div>
           </div>
-          <div className="mb-8">
-            <Summary
-              data={[
-                { title: 'Ticker', value: data.ticker.string, icon: icon_info },
-                { title: 'Description', value: data.description.string, icon: icon_info },
-              ]}
-            />
+          <div className="flex flex-row gap-2">
+            <div className="f-full">
+              <div className="mx-auto mb-5 bg-white p-2">
+                <img src={imageUrl}/>
+              </div>
+            </div>
+            <div className="mb-5 w-full">
+              <div className="mb-2 w-full relative">
+                <img src={iconUrl} className="w-20 h-20 absolute top-2 right-2" alt=""/>
+                <Summary
+                  data={[
+                    { title: 'Ticker', value: data.ticker.string, icon: icon_info },
+                    { title: 'Name', value: data.name.string, icon: icon_info },
+                    { title: 'Description', value: data.description.string, icon: icon_info },
+                  ]}
+                />
+              </div>
+              <div className="mb-2 w-full">
+                <Summary
+                  data={[
+                    { title: 'Creator', value: data.creator || '-', icon: icon_info },
+                    { title: 'Owner', value: data.owner || '-', icon: icon_info },
+                  ]}
+                />
+              </div>
+              <div className="mb-2 w-full">
+                <Summary
+                  data={[
+                    { title: 'Metadata', value: metadataUrl || '-', icon: icon_info },
+                  ]}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </Hero>
-      <div className="max-w-6xl w-full px-5 md:mx-auto">
-        <InfoExpand title={"Additional info & Documentation"}>
-          <div className="max-w-2xl">
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>Ticker</div>
-              <div>{data.ticker.string}</div>
-            </div>
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>Name</div>
-              <div>{data.name.string}</div>
-            </div>
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>Owner</div>
-              <div>{data.owner}</div>
-            </div>
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>Creator</div>
-              <div>{data.creator}</div>
-            </div>
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>description</div>
-              <div>{data.description.string}</div>
-            </div>
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>Additional metadata uri</div>
-              <div>{data.additional_metadata_uri.string}</div>
-            </div>
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>media_uri</div>
-              <div>{data.media_uri.string}</div>
-            </div>
-            <div className="flex gap-2 bg-white py-1 px-3 my-2 justify-between">
-              <div>icon_uri</div>
-              <div>{data.icon_uri.string}</div>
-            </div>
-          </div>
-        </InfoExpand>
-      </div>
     </>
   );
 }
