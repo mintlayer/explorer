33bce052c2d3572823f3060753259f5d0c4f9463
Sergey Chystiakov <anyxem@gmail.com>
2025-03-17 10:27:49 +0100

    update subsidy table

--- src/utils/emission.ts
+++ src/utils/emission.ts
@@ -1,22 +1,23 @@
 export const EMISSION_TABLE = [
-  { start: 0      , end: 262800 , reward: 202 },
-  { start: 262801 , end: 525600 , reward: 151 },
-  { start: 525601 , end: 788400 , reward: 113 },
-  { start: 788401 , end: 1051200, reward: 85 },
-  { start: 1051201, end: 1314000, reward: 64 },
-  { start: 1314001, end: 1576800, reward: 48 },
-  { start: 1576801, end: 1839600, reward: 36 },
-  { start: 1839601, end: 2102400, reward: 27 },
-  { start: 2102401, end: 2365200, reward: 20 },
-  { start: 2365201, end: 2628000, reward: 15 },
-  { start: 2628001, end: Infinity,reward: 0 }
+  { range: [0      , 262800  ], reward: 202 },
+  { range: [262801 , 525600  ], reward: 151 },
+  { range: [525601 , 788400  ], reward: 113 },
+  { range: [788401 , 1051200 ], reward: 85 },
+  { range: [1051201, 1314000 ], reward: 64 },
+  { range: [1314001, 1576800 ], reward: 48 },
+  { range: [1576801, 1839600 ], reward: 36 },
+  { range: [1839601, 2102400 ], reward: 27 },
+  { range: [2102401, 2365200 ], reward: 20 },
+  { range: [2365201, 2628000 ], reward: 15 },
+  { range: [2628001, Infinity], reward: 0 }
 ];
 
 export function get_total_subsidy(blockNumber: number) {
   let total = 0;
   let previousEnd = 0;
 
-  for (const { start, end, reward } of EMISSION_TABLE) {
+  for (const { range, reward } of EMISSION_TABLE) {
+    const [start, end] = range;
     if (blockNumber < start) {
       break;
     }
@@ -30,14 +31,14 @@ export function get_total_subsidy(blockNumber: number) {
 }
 
 export const block_subsidy_at_height = (block_height: number) => {
-  const current = EMISSION_TABLE.findIndex((r) => r.start <= block_height && r.end >= block_height);
+  const current = EMISSION_TABLE.findIndex(({ range }) => range[0] <= block_height && range[1] >= block_height);
   return EMISSION_TABLE[current]?.reward;
 }
 
 export const get_annual_subsidy =  (block_height: number) => {
-  const current = EMISSION_TABLE.findIndex((r) => r.start <= block_height && r.end >= block_height);
+  const current = EMISSION_TABLE.findIndex(({ range }) => range[0] <= block_height && range[1] >= block_height);
   const current_subsidy = EMISSION_TABLE[current]?.reward;
-  const current_subsidy_end = EMISSION_TABLE[current]?.end || Infinity;
+  const current_subsidy_end = EMISSION_TABLE[current]?.range[1] || Infinity;
   const future_subsidy = EMISSION_TABLE[current + 1]?.reward || 0;
   const total_blocks_in_year = 720 * 365;
   const current_subsidy_part = current_subsidy === 0 ? 0 : current_subsidy * (current_subsidy_end - block_height);
@@ -46,9 +47,9 @@ export const get_annual_subsidy =  (block_height: number) => {
 }
 
 export const get_annual_subsidy_delegator =  (block_height: number, pool: any, part: any) => {
-  const current = EMISSION_TABLE.findIndex((r) => r.start <= block_height && r.end > block_height);
+  const current = EMISSION_TABLE.findIndex(({ range }) => range[0] <= block_height && range[1] > block_height);
   const current_reward = (EMISSION_TABLE[current].reward - pool.cost_per_block) * (1 - pool.margin_ratio) * part;
-  const current_reward_end = EMISSION_TABLE[current].end || Infinity;
+  const current_reward_end = EMISSION_TABLE[current].range[1] || Infinity;
   const future_reward = EMISSION_TABLE[current + 1] && EMISSION_TABLE[current + 1].reward ? (EMISSION_TABLE[current + 1].reward - pool.cost_per_block) * (1 - pool.margin_ratio) * part : 0;
   const total_blocks_in_year = 720 * 365;
   const current_reward_part = current_reward === 0 ? 0 : current_reward * (current_reward_end - block_height);
