975087afbfcd4b7130a4536cf9a9c40e839e98ac
Sergey Chystiakov <anyxem@gmail.com>
2026-06-20 14:46:32 +0200

    feat: revert stake button

--- src/app/(homepage)/_components/summary/index.tsx
+++ src/app/(homepage)/_components/summary/index.tsx
@@ -2,10 +2,20 @@ import { HeadingBox } from "@/app/(homepage)/_components/heading_box";
 import community from "@/app/(homepage)/_icons/24px/community.svg";
 import txs from "@/app/(homepage)/_icons/24px/txs.svg";
 import { Icon } from "@/app/_components/heading_box/icon";
+import {formatML} from "@/utils/numbers";
 
 export function Summary({ data, data_transaction }: { data: any; data_transaction: number }) {
   return (
     <>
+      <div className="col-span-2">
+        <HeadingBox
+          title={`${formatML(data.total_amount)} ML`}
+          subtitle="Total Stake Amount"
+          icon={<Icon src={community} />}
+          button={{ label: "Start staking", link: "/pools" }}
+          iconTooltip="Current APY"
+        />
+      </div>
       <div>
         <HeadingBox title={data.validators_count} subtitle="Validators" icon={<Icon src={community} />} iconTooltip="Validators" />
       </div>
